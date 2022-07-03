4da68e16c8e185e27619af363ccdd6c616b618f2
Omer Furkan <omerfurkansen@gmail.com>
2022-07-04 01:02:05 +0300

    refactor: increase modularity

--- src/common/TextAnimations.ts
+++ src/common/TextAnimations.ts
@@ -1,27 +1,26 @@
 const INITIAL_OBJECT_ANIMATION_DURATION = 1000;
-const MMP_TEXT = 'Make My Persona';
+export const MMP_TEXT = 'Make My Persona';
 
-const fallIn = {
+export const fallIn = {
   from: { opacity: 0, transform: 'translate3d(0,-100%,0)' },
   to: { opacity: 1, transform: 'translate3d(0,0,0)' },
   config: { duration: INITIAL_OBJECT_ANIMATION_DURATION },
 };
 
-const rotateAndFall = {
+const rotateAndFallBase = {
   from: { opacity: 0, transform: 'translate3d(0,-300%,0) rotate(-60deg)' },
   to: { opacity: 1, transform: 'translate3d(0,0,0) rotate(0deg)' },
   config: { mass: 5, tension: 8000, friction: 200 },
 };
 
-const fadeIn = {
+export const rotateAndFall = {
+  lengthOfText: MMP_TEXT.length,
+  delay: MMP_TEXT.split('').map((t, i) => ({ ...rotateAndFallBase, delay: INITIAL_OBJECT_ANIMATION_DURATION + 150 * i }))
+};
+
+export const fadeIn = {
   from: { opacity: 0 },
   to: { opacity: 1 },
   config: { duration: 1000 },
-  delay: INITIAL_OBJECT_ANIMATION_DURATION + MMP_TEXT.split('').length * 150,
+  delay: INITIAL_OBJECT_ANIMATION_DURATION + MMP_TEXT.length * 150,
 };
-
-export const TextAnimations = {
-  fallIn,
-  rotateAndFall,
-  fadeIn,
-}
